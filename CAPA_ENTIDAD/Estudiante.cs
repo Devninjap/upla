5ae14fbf1a6779d4c616ddcc4cb5eb6964b8061d
Yampier Porras <yampierporras@gmail.com>
2017-05-06 16:57:15 -0500

    yampier 1

---
+++ CAPA_ENTIDAD/Estudiante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_ENTIDAD
+{
+    public class Estudiante
+    {
+        public int codEst { get; set; }
+    }
+}
